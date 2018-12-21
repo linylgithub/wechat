bebf114879ca73ce888c417b05a7007e65cdebac
linyl <lylyoung@outlook.com>
2018-12-21 16:29:20 +0800

    增加微信认证代码

--- main.py
+++ main.py
@@ -7,4 +7,35 @@
 # Description:
 """
 
-import web
+import hashlib
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/wx')
+def wx():
+    try:
+        data = request.args
+        if len(data) == 0:
+            return 'hello, this is handle view'
+        signature = data.get('signature')
+        timestamp = data.get('timestamp')
+        nonce = data.get('nonce')
+        echostr = data.get('echostr')
+        token = 'I7aPyH4HMzajIUKy4rY1OJGB4xesIJ5o'
+        
+        list = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
+        print "API:/wx GET func: hashcode: %s, signature: %s" % (hashcode, signature) 
+        if hashcode == signature:
+            return echostr
+        else:
+            return ""
+    except Exception, Argument:
+        return Argument
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=2333, debug=True)
